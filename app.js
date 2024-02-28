265909d7efeaab042ac732ea94975715a7ca9d11
devjoowon <dev.joowon@gmail.com>
2024-02-28 10:04:12 +0900

    프로젝트 초기 설정

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT;
+const db = require("./models");
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use("/static", express.static(__dirname + "/static"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
+
+app.get("*", (req, res) => {
+  res.render("404");
+});
+
+db.sequelize.sync({ force: false }).then((result) => {
+  console.log("DB연결 성공");
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
---
+++ models/index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const Sequelize = require("sequelize");
+const config = require(__dirname + "/../config/config.js")["development"];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
