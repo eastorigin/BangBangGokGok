508b41e2d114bf3c2a23d9216a68dbc5b9633ee3
eastorigin <eastorigin21@gmail.com>
2024-03-01 12:14:20 +0900

    feat : [FE] signin.ejs 작업 중

--- controller/Cuser.js
+++ controller/Cuser.js
@@ -3,6 +3,10 @@ exports.getSignin = (req, res) => {
     res.render("user/signin");
 };
 
+exports.getSignup = (req, res) => {
+    res.render("signup");
+};
+
 exports.postSignin = (req, res) => {
     User.postSignin(req.body, (result) => {
         if (result.length > 0) {
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,10 @@
 // 유저 관련 라우터
 router.get("/signin", controller.getSignin);
 
+router.get("/signup", controller.getSignup);
+
 router.post("/signin", controller.postSignin);
+
+router.post("/signup", controller.postSignup);
 
 module.exports = router;
