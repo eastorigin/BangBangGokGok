fdeeacb255864d2764d337995bc1e1a28924b1e2
devjoowon <dev.joowon@gmail.com>
2024-03-05 14:26:40 +0900

    feat : [공통] config.js 로컬, 배포 수정

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 const development = {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.DB_LOCAL_USERNAME,
+    password: process.env.DB_LOCAL_PASSWORD,
+    database: process.env.DB_LOCAL_DATABASE,
+    host: process.env.DB_LOCAL_HOST,
     dialect: "mysql",
 };
 
@@ -12,7 +12,7 @@ const prod = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: "49.50.164.79",
+    host: process.env.DB_HOST,
     dialect: "mysql",
 };
 
