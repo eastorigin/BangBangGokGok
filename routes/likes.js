f0ea82a14180d479310bc79f0c60abdf9564ec50
eastorigin <eastorigin21@gmail.com>
2024-03-10 21:23:28 +0900

    feat : [FE] 찜 버튼 완료

--- routes/likes.js
+++ routes/likes.js
@@ -5,4 +5,11 @@ const router = express.Router();
 router.post("/", controller.postLikes);
 router.delete("/", controller.deleteLikes);
 
+// router 파일에 좋아요 수 조회 경로 추가
+
+router.get("/:p_seq", controller.getLikesCount); // 게시물 ID를 URL 파라미터로 받는 경로 설정
+
+// 좋아요 상태 조회 경로 추가
+router.get("/status/:p_seq", controller.checkLikeStatus);
+
 module.exports = router;
